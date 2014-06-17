e45aa95c621fd09bed5dff5f0ae4ada14079dc25
Pouyan <popeno2003@gmail.com>
2014-06-17 09:50:18 +0200

    some changes

--- LoginShibbolethAuth.php
+++ LoginShibbolethAuth.php
@@ -55,8 +55,6 @@ class LoginShibbolethAuth extends \Piwik\Plugins\Login\Auth
      */
     public function authenticate()
     {
-        if (is_null($this->login)) {
-            var_dump($this->login);
          if(isset($_SERVER["REMOTE_USER"])){
               $this->login = $_SERVER["REMOTE_USER"];
               $this->password = '';
@@ -64,8 +62,7 @@ class LoginShibbolethAuth extends \Piwik\Plugins\Login\Auth
               $user = $model->getUser($this->login);
               $code = $user['superuser_access'] ? AuthResult::SUCCESS_SUPERUSER_AUTH_CODE : AuthResult::SUCCESS;
               return new AuthResult($code, $this->login, $this->token_auth);
-            }
-         }
+        }
          else if (!empty($this->login)) {
          $login = $this->login;
 
